92129fac8e37957d7d0f380a94202d3894a83b5e
Andres03 <andrezztorregrosa@gmail.com>
2020-09-20 12:30:47 -0500

    agregando tareas gulp

--- Gruntfile.js
+++ Gruntfile.js
@@ -139,11 +139,20 @@ module.exports = function(grunt){
 
 
 
-    grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-browser-sync');
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
+   
 	grunt.registerTask('css', ['sass']);
 	grunt.registerTask('default', ['browserSync', 'watch'])
 	grunt.registerTask('img:compress', ['imagemin']);
+	grunt.registerTask('build', [
+		'clean',
+		'copy',
+		'imagemin',
+		'useminPrepare',
+		'concat',
+		'cssmin',
+		'uglify',
+		'filerev',
+		'usemin'
+
+		])
 };
